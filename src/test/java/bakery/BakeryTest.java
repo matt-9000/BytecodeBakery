df311bcade57dc9f5285fbb219049cc1ae5b8be7
matt-9000 <33230636+matt-9000@users.noreply.github.com>
2017-11-01 10:09:28 -0500

    Added BakeryTest stub

---
+++ src/test/java/bakery/BakeryTest.java
@@ -0,0 +1,18 @@
+package bakery;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BakeryTest {
+
+    @Test
+    public void testBakery() {
+
+        // TODO: Pipe in System.in from file, pipe out System.out to validate output
+        // Bakery.main(null);
+        
+        assertTrue(true);
+    }
+    
+    
+}
